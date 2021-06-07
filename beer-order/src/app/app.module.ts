25c15ab68ef5e70376187d720247676d14144ca0
Laurent Sewer <sewer.laurent@hotmail.com>
2021-06-07 13:20:11 +0200

    angular automatics changements

--- beer-order/src/app/app.module.ts
+++ beer-order/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { NotifComponent } from './notif/notif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    NotifComponent
   ],
   imports: [
     BrowserModule
